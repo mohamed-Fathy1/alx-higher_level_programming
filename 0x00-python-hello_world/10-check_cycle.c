57bb5c384aad41c3d091b3e61ef6e3dbf3e548f1
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-05-08 20:15:45 -0700

    Add task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,10 +1,21 @@
 #include "lists.h"
+#include <stdio.h>
 /**
  * check_cycle - function
  * @list: input
  * Return: int
  */
-int check_cycle(listint_t *list __attribute__((unused)))
+int check_cycle(listint_t *list)
 {
+	listint_t *head = list;
+	listint_t *ptr;
+
+	ptr = list->next;
+	while (ptr)
+	{
+		ptr = ptr->next;
+		if (ptr == head)
+			return (1);
+	}
 	return (0);
 }
