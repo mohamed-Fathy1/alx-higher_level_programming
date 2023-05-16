04ef5af271b34c9c23a8aa84af43fece5d7a1c86
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-05-16 14:21:33 -0700

    Add task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,29 +10,29 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *ptr1 = *head;
 	int len = 0;
-	int half_list;
 
 	while (ptr1)
 	{
 		ptr1 = ptr1->next;
 		len++;
 	}
-
-	half_list = len / 2;
+	const int const_len = len;
+	int arr[const_len];
+	int x = 0;
 
 	ptr1 = *head;
-	while (len != half_list)
+	while (ptr1)
 	{
-		int i = 0;
-		listint_t *ptr2 = *head;
+		arr[x] = ptr1->n;
+		ptr1 = ptr1->next;
+		x++;
+	}
+	ptr1 = *head;
+	int c = 0;
 
-		while (i != len - 1)
-		{
-			ptr2 = ptr2->next;
-			i++;
-		}
-		len--;
-		if (ptr1->n != ptr2->n)
+	while (c != len)
+	{
+		if (ptr1->n != arr[--len])
 			return (0);
 		ptr1 = ptr1->next;
 	}
