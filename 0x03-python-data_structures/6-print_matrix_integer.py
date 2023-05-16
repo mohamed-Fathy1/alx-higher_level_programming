9da4351bc76784ea245342d023270c921feaa307
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-05-15 18:37:50 -0700

    Add task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     if type(matrix) is list:
         for i in matrix:
-            print(*i)
+            for j in i:
+                print("{:d}".format(j), end=" " if j != i[-1] else "")
+            print()
