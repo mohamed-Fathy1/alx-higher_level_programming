ec6b4f0ba97dbc3bbeb4df755ecaf2efccd76bcc
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-05-15 19:14:05 -0700

    Add task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     length = len(my_list)
     if length == 0:
         return (None)
-    tmp_max = 0
+    tmp_max = my_list[0]
     for i in my_list:
         if i > tmp_max:
             tmp_max = i
