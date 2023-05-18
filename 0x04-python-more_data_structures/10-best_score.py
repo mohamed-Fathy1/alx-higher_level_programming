68b0bf27045eb85498464aea2f57c84a31ddd667
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-05-18 02:23:07 -0700

    Add task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return (sorted(a_dictionary.items())[len(a_dictionary) - 1][0])
-    return (None)
+        return (max(a_dictionary, key=a_dictionary.get))
