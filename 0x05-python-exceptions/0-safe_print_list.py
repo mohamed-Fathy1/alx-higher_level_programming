4d1033789ab7ad8f009faba9d0a59b905264c244
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-05-29 19:18:56 -0700

    Add task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for item in my_list:
         try:
-            if i > x:
+            if i >= x:
                 raise IndexError
             i += 1
             print("{}".format(item), end="")
