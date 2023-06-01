23b568cb06e03deddfd2df317326deee4598a1bf
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-05-31 19:12:53 -0700

    Add taask 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,8 +5,8 @@
 class Square:
     """quare iner class"""
     def __init__(self, size=0):
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        else if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
