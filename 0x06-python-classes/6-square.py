fec28f8d5c062add4fe6cd3e9ed1ec5d3e7e8596
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-06 02:19:37 -0700

    Add task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -47,7 +47,7 @@ class Square:
             return
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print("", end="") for i in range(0, self.__position[0])]
+            [print(" ", end="") for i in range(0, self.__position[0])]
             for j in range(self.__size):
                 print("#", end="")
             print()
