e60c5dbc704cd9722ff9491b27bb89c2d674ec1d
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-13 19:51:31 -0700

    Add task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == '__main__':
-    my_list = load_from_json_file("add_item.json")
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
+
     for i in range(1, len(sys.argv)):
         my_list.append(sys.argv[i])
 
