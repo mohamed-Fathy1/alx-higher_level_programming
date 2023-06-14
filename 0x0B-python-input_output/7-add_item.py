fcc2bba65ca539c55d73268285b983c41300f010
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-13 19:48:12 -0700

    Add task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,14 +2,12 @@
 '''module main'''
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-from_json_string = __import__('4-from_json_string').from_json_string
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == '__main__':
-    my_list = []
+    my_list = load_from_json_file("add_item.json")
     for i in range(1, len(sys.argv)):
         my_list.append(sys.argv[i])
 
     save_to_json_file(my_list, "add_item.json")
-    with open("add_item.json", )
-    from_json_string()
