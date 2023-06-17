8170fe0ee691d86050c2e2c105a8b058f24037b1
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-17 01:50:41 -0700

    Add task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,12 @@ def text_indentation(text):
     """Function"""
     if type(text) is not str:
         raise TypeError("text must be a string")
+    skip = 0
     for i in text:
+        if (i == " ") and (skip == 1):
+            continue;
+        skip = 0
         print(i, end="")
         if i in ('.', '?', ':'):
+            skip = 1
             print("\n")
