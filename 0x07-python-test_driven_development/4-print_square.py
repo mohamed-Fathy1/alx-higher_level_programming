5b764db9001b19296e7b494c58e7a5b0fc6c0d5e
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-17 01:09:23 -0700

    Add task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,11 +5,10 @@
 def print_square(size):
     """Function"""
     if type(size) is not int:
-        raise TypeError('size must be an integer')
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError('size must be >= 0')
-
+        raise ValueError("size must be >= 0")
     for i in range(size):
         for j in range(size):
-            print('#', end="")
+            print("#", end="")
         print()
