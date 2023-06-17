87811c1660c2773342d7bab8c0ffdc3e9c8e42e8
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-16 21:52:06 -0700

    Add task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,30 +4,23 @@
 
 def matrix_divided(matrix, div):
     """Function divides"""
-    new_matrix = []
+    matrix_copy = []
 
-    if type(matrix) is not list:
-        raise TypeError('matrix must be a matrix (list of lists)' +
-                        ' of integers/floats')
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
+    if matrix and not isinstance(matrix[0], list):
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
+    size = len(matrix[0])
     for i in matrix:
-        if type(i) is not list:
-            raise TypeError('matrix must be a matrix (list of lists)' +
-                            ' of integers/floats')
-        if len(i) is not len(matrix[0]):
-            raise TypeError('Each row of the matrix must have the same size')
-        for ele in i:
-            if (type(ele) is not int) and (type(ele) is not float):
-                raise TypeError('matrix must be a matrix (list of lists)' +
-                                ' of integers/floats')
-    if (type(div) is not int) and (type(div) is not float):
-        raise TypeError('div must be a number')
-    elif div is 0:
-        raise ZeroDivisionError('division by zero')
-
-    for i in matrix:
-        new_row = []
-        for val in i:
-            quot = round((val / div), 2)
-            new_row.append(quot)
-        new_matrix.append(new_row)
-    return new_matrix
+        tmp = []
+        if not isinstance(i, list):
+            raise TypeError("matrix must be a matrix (list of lists)"
+                            " of integers/floats")
+        if size != len(i):
+            raise TypeError("Each row of the matrix must have the same size")
+        for j in i:
+            tmp.append(round(j / div, 2))
+        matrix_copy.append(tmp)
+    return matrix_copy
