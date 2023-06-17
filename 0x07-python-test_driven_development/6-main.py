127c67e3e10c3f27bfd44c813682c08efcf17155
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-17 12:00:07 -0700

    Add task 6

---
+++ 0x07-python-test_driven_development/6-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+max_integer = __import__('6-max_integer').max_integer
+
+print(max_integer([1, 2, 3, 4]))
+print(max_integer([1, 3, 4, 2]))
+print(max_integer([-1, -3, -4, -2]))
+print(max_integer([-1.4, 3.6, 4.9, 2.212]))
+print(max_integer([1]))
+print(max_integer([]))
+print(max_integer())
+print(max_integer("jghfhgjf"))
+print(max_integer(1))
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,44 +5,31 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    ''' TestCase class '''
 
-    def test_list_length(self):
-        """test"""
+    def test_integer(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 2]), 3)
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_ngative(self):
+        self.assertEqual(max_integer([-1, -3, -4, -2]), -1)
+
+    def test_floating_numbers(self):
+        self.assertAlmostEqual(max_integer([-1.4, 3.6, 4.9, 2.212]), 4.9)
+
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer(()), None)
-        self.assertEqual(max_integer('a'), 'a')
-        self.assertRaises(TypeError, max_integer, (5))
-        self.assertEqual(max_integer(''), None)
-        self.assertEqual(max_integer([5]), 5)
 
-    def test_higher_list_dimensions(self):
-        """test function"""
-        self.assertEqual(max_integer([[[8, 2], [2], []], [[4], [5]], [[6]]]),
-                         [[8, 2], [2], []])
-        self.assertEqual(max_integer([{5, 4}, {6, 3, 1}, {20}]), {4, 5})
-        self.assertEqual(max_integer([[5, 4, 1], [6, 3], []]), [6, 3])
-        self.assertEqual(max_integer([(5, 4, 1), (6, 3), ()]), (6, 3))
-        self.assertEqual(max_integer([[[1, 2], [2], []], [[4], [5]], [[6]]]),
-                         [[6]])
-        self.assertEqual(max_integer(['azzz', 'byy', 'cx']), 'cx')
+    def test_no_argument(self):
+        self.assertEqual(max_integer(), None)
 
-    def test_first_list_dimension_type(self):
-        """test """
-        self.assertEqual(max_integer(['a', 'b']), 'b')
-        self.assertRaises(TypeError, max_integer, [3, 'a'])
-        self.assertEqual(max_integer([-1, -.5, False]), False)
-        self.assertEqual(max_integer([5, .544, True]), 5)
-        self.assertEqual(max_integer([3, 5.0]), 5.0)
-        self.assertEqual(max_integer([3, -5.3456]), 3)
+    def test_string(self):
+        self.assertEqual(max_integer("jghfhgjf"), "j")
 
-    def test_list_type(self):
-        """Test function"""
-        self.assertRaises(TypeError, max_integer, 5)
-        self.assertRaises(TypeError, max_integer, True)
-        self.assertEqual(max_integer((3, 5)), 5)
-        self.assertEqual(max_integer('actg'), 't')
-        self.assertRaises(TypeError, max_integer, {3, 5})
-        self.assertEqual(max_integer([3, 5]), 5)
+    def test_not_iteratable(self):
+        with self.assertRaises(TypeError):
+            max_integer(1)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
