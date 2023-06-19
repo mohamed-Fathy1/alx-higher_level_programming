c4766a6f18aa8f1ce2ad12145361b95045248c81
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-19 16:10:44 -0700

    Add task 0

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -18,6 +18,25 @@ class TestBase(unittest.TestCase):
         base2 = Base()
         self.assertEqual(base1.id, base2.id - 1)
 
+    def test_None_id(self):
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_unique_id(self):
+        self.assertEqual(12, Base(12).id)
+
+    def test_nb_instances_after_unique_id(self):
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 1)
+
+    def test_id_public(self):
+        b = Base(12)
+        b.id = 9
+        self.assertEqual(9, b.id)
+
 
 if __name__ == '__main__':
     unittest.main()
