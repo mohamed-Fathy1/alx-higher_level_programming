438b9ac531955c2ee20aafc0435dd82e2bb8980a
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-19 16:06:23 -0700

    Add test for base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,18 +7,16 @@ import unittest
 class TestBase(unittest.TestCase):
     '''test Base class'''
 
-    def setUp(self) -> None:
-        '''run on every test fuction call'''
-        self.base2 = Base()
-        self.base1 = Base(12)
-        self.base3 = Base()
-        self.base4 = Base()
-        self.base5 = Base()
-
     def test_id(self):
-        '''test id'''
-        self.assertEqual(self.base2.id, 1)
-        self.assertEqual(self.base1.id, 12)
-        self.assertEqual(self.base3.id, 2)
-        self.assertEqual(self.base4.id, 3)
-        self.assertEqual(self.base5.id, 4)
+        base1 = Base(12)
+        base2 = Base(24)
+        self.assertEqual(base1.id, 12)
+        self.assertEqual(base2.id, 24)
+
+    def test_no_args(self):
+        base1 = Base()
+        base2 = Base()
+        self.assertEqual(base1.id, base2.id - 1)
+
+if __name__ == '__main__':
+    unittest.main()
