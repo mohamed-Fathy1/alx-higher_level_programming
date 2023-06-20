abfe5942bbf0111e6ed959991a66948dec9d46b8
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-20 00:10:42 -0700

    Add task 9

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,3 +12,16 @@ class Square(Rectangle):
         '''print object structer'''
         return (f"[Square] ({self.id}) {self.x}/{self.y} - "
                 f"{self.width}")
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = self.height = value
