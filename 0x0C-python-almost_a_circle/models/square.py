d71c4a6b1afadd25b7982cc05f3a278961123f47
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-06-20 00:00:39 -0700

    Add task 9

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,58 +5,10 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Represent a square"""
-
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new square"""
         super().__init__(size, size, x, y, id)
 
-    @property
-    def size(self):
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        self.width = value
-        self.height = value
-
-    def update(self, *args, **kwargs):
-        if args and len(args) != 0:
-            count = 0
-            for arg in args:
-                if count == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif count == 1:
-                    self.size = arg
-                elif count == 2:
-                    self.x = arg
-                elif count == 3:
-                    self.y = arg
-                count += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
-
-    def to_dictionary(self):
-        return {
-            "id": self.id,
-            "size": self.width,
-            "x": self.x,
-            "y": self.y
-        }
-
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        '''print object structer'''
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - "
+                f"{self.width}")
