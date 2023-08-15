7eeb4e13a6d77bf4685870ee4bb40f53731d74f5
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-08-15 07:30:36 +0300

    Add taask 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,12 +3,11 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  const arr = process.argv.slice(2);
-  arr.sort();
-  const newArr = [];
-  for (const x in [...arr]) {
-    newArr[x] = Number(arr[x]);
+  const arr = process.argv.slice(2).sort();
+  for (const x of [...arr]) {
+    arr.shift();
+    arr.push(Number(x));
   }
-  const maxVal = Math.max(...newArr);
-  console.log(newArr[newArr.indexOf(maxVal) - 1]);
+  const maxVal = Math.max(...arr);
+  console.log(arr[arr.indexOf(maxVal) - 1]);
 }
