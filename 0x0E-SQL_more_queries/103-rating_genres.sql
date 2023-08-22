96b667fccd36c388d3278ca4f3affd6927574645
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-08-22 15:48:43 +0300

    Add task 103

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -7,6 +7,6 @@ JOIN (
     FROM tv_shows JOIN tv_show_ratings
     ON tv_shows.id = tv_show_ratings.show_id
     GROUP BY tv_shows.id
-) as Y ON Y.id = tv_show_genres.show_id
+) AS Y ON Y.id = tv_show_genres.show_id
 GROUP BY name
 ORDER BY rating DESC;
