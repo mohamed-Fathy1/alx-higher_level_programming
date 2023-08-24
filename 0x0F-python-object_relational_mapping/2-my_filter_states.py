9aa486f1dcc2117bbe7dbdeccaa7fab175ecad8f
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-08-24 04:48:16 +0300

    Add task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
-Write a script that lists all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa:
+Write a script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
 '''
 import MySQLdb
 from sys import argv
