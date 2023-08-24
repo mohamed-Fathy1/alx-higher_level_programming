9f2952f5a9365b9105c09885b3bd9707c890b18b
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-08-24 04:49:35 +0300

    Add task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3], charset="utf8")
 
     cur = db.cursor()
-    cur.execute('''SELECT * FROM states WHERE name = %s
-                ORDER BY id''', (argv[4],))
+    cur.execute('''SELECT * FROM states WHERE name = {}
+                ORDER BY id'''.format(argv[4]))
     for row in cur._rows:
         print(row)
