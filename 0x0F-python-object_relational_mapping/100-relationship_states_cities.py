2adeca0546e8ab5524c150dacfee7d7ca0c1fae0
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-08-26 00:13:14 +0300

    Add taskl 100

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from relationship_city import City
+
+from sqlalchemy.orm import Session
+from relationship_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    new_city = City(name='San Francisco')
+    new_state = State(name='California')
+    new_state.cities.append(new_city)
+    session.add(new_city)
+    session.add(new_state)
+    session.commit()
