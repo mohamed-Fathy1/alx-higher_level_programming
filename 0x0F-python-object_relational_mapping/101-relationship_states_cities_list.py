c584561f36c1b2f16debc07dbe55e1422f9aafd3
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-08-27 17:01:05 +0300

    Add task 101

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
 
     session = Session(engine)
 
-    query = session.query(State).join(City).order_by(State.id, City.id)
+    query = session.query(State).join(
+        City, isouter=True).order_by(State.id, City.id)
     for state in query:
         print(f"{state.id}: {state.name}")
         for city in state.cities:
