85cafc2e7f9a474123c31bee890fbeded0e9b1f8
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-09-03 01:43:01 +0300

    Add task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,14 +3,11 @@
 
 
 def find_peak(list_of_integers):
-    # if not list_of_integers:
-    #     return None
     start, end = 0, len(list_of_integers) - 1
-    # if (len(list_of_integers) == 1):
-    #     return list_of_integers[0]
     while (start <= end):
         mid = start + ((end - start) // 2)
-        if (mid < len(list_of_integers) - 1) and (list_of_integers[mid + 1] > list_of_integers[mid]):
+        if ((mid < len(list_of_integers) - 1)
+                and (list_of_integers[mid + 1] > list_of_integers[mid])):
             start = mid + 1
         elif (mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]):
             end = mid - 1
