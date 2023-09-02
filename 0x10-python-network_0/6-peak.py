092afcc8201b717b853191a78cfbb915d435c4e2
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-09-03 02:36:24 +0300

    Add tassk 2

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,24 @@
 
 def find_peak(list_of_integers):
     ''' Function that finds a peak in a list of unsorted integers '''
-    if list_of_integers == []:
+    if not list_of_integers:
         return None
-    list_of_integers.sort()
-    return list_of_integers[-1]
+    if list_of_integers[0] > list_of_integers[1]:
+        return list_of_integers[0]
+    if list_of_integers[-1] > list_of_integers[-2]:
+        return list_of_integers[-1]
+
+    i = 1
+    while i < len(list_of_integers) - 1:
+        prev = list_of_integers[i - 1]
+        curr = list_of_integers[i]
+        next = list_of_integers[i + 1]
+        if i > 2 and prev >= curr and prev >= list_of_integers[i - 2]:
+            return prev
+        if curr >= prev and curr >= next:
+            return curr
+        if (i < len(list_of_integers) - 2 and next >= curr
+                and next >= list_of_integers[i + 2]):
+            return prev
+
+        i += 2
