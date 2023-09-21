ff18d5a162e23bea05737bc9bc891d2fb91aaa02
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-09-21 15:29:03 +0300

    Add task 90

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,10 +7,10 @@ import urllib.request
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
-        content_type = response.headers.get(
-            "Content-Type").split("charset=")[-1]
+        # content_type = response.headers.get(
+        # "Content-Type").split("charset=")[-1]
         print('Body response:')
         print(f'\t- type: {type(response.msg.encode())}')
         print(f'\t- content: {response.msg.encode()}')
         print(
-            f'\t- {content_type} content: {response.msg}')
+            f'\t- utf8 content: {response.msg}')
