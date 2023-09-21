d8def09dd303d75b7811cce4295d251592d74306
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-09-21 15:17:06 +0300

    Add task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Write a Python script that fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content_type = response.headers.get(
+            "Content-Type").split("charset=")[-1]
+        print('Body response:')
+        print(f'\t- type: {type(response.msg.encode())}')
+        print(f'\t- content: {response.msg.encode()}')
+        print(
+            f'\t- {content_type} content: {response.msg}')
