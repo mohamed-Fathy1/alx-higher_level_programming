42478282bd91d70f1331f3daf96e5757ee8c5b39
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-09-22 21:16:13 +0300

    Add taadk 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ Python script that fetches https://intranet.hbtn.io/status
 """
 import requests
 
-response = requests.get('https://alx-intranet.hbtn.io/status')
+response = requests.get('http://0.0.0.0:5050/status')
 print('Body response:')
-print('\t- type:', type(response.reason))
-print('\t- content:', response.reason)
+print('\t- type:', type(response.text))
+print('\t- content:', response.text)
