8f42499e9aaec1b41977ff7ae18698d5bbdb514f
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-09-22 21:17:17 +0300

    Add tasl 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ Python script that fetches https://intranet.hbtn.io/status
 """
 import requests
 
-response = requests.get('http://0.0.0.0:5050/status')
+response = requests.get('https://alx-intranet.hbtn.io/status')
 print('Body response:')
 print('\t- type:', type(response.text))
 print('\t- content:', response.text)
