a1941908432655a7724710e6ebb8e222fa7a7dd8
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-09-22 21:43:04 +0300

    Add taak 8

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the value of the
+variable X-Request-Id in the response header
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.post(argv[1])
+
+    if response.ok:
+        print(response.text)
+    else:
+        print('Error code:', response.status_code)
