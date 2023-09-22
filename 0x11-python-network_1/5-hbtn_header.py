8ade445190c22c9fc45c066a1f2b63a569c55141
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-09-22 21:28:43 +0300

    Add  tas k

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,5 +6,6 @@ variable X-Request-Id in the response header
 import requests
 from sys import argv
 
-response = requests.get(argv[1])
-print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    print(response.headers.get('X-Request-Id'))
