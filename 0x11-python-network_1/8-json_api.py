0a0a094159488ea3ec8671b7b8401afe1c004434
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-09-23 15:14:56 +0300

    Add task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+takes in a URL, sends a request to the URL and displays the value of the
+variable X-Request-Id in the response header
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    payload = {'q': argv[1] if argv[1] else ""}
+    response = requests.post('http://0.0.0.0:5000/search_user', data=payload)
+    res = response.json()
+    if isinstance(res, dict):
+        if res:
+            print(f"[{res['id']}] {res['name']}")
+        else:
+            print("No result")
+    else:
+        print("Not a valid JSON")
