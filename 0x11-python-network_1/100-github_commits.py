a628f46542eb2f555de72263bc5455f0b6510f1a
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-09-23 17:59:36 +0300

    Add task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,9 +11,12 @@ if __name__ == "__main__":
     response = requests.get(
         f'https://api.github.com/repos/{argv[1]}/{argv[2]}/commits')
     res = response.json()
-    i = 0
-    while 10 > i < len(res):
-        print("{}: {}".format(
+    try:
+        i = 0
+        while 10 > i < len(res):
+            print("{}: {}".format(
                 res[i].get("sha"),
                 res[i].get("commit").get("author").get("name")))
-        i += 1
+            i += 1
+    except IndexError:
+        pass
