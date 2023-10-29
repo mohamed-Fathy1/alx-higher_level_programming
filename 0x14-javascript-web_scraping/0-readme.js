0a98d91945082b01c08f658e6df86bf5a59e2ebf
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-29 20:30:35 +0200

    Add task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 // print the content of a file
 const fs = require('fs');
 
-const FILE_NAME = process.argv[1];
+const FILE_NAME = process.argv[2];
 
 fs.readFile(FILE_NAME, 'utf8', (error, data) => {
   if (error) { console.log(error); } else { console.log(data); }
