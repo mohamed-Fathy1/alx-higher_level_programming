4e44ea4a9df765bab9c81b2f1f59ac721bd96288
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-29 21:12:22 +0200

    Add task 2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -9,6 +9,6 @@ request.get(`https://swapi-api.alx-tools.com/api/films/${id}`, (error, res, body
   if (error) {
     console.error('error:', error);
   }
-  const title = JSON.parse(body)["title"];
+  const title = JSON.parse(body).title;
   console.log(title);
 });
