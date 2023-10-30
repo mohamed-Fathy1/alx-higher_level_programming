bcadaf1bc2c1c3cb790fcc0ac26661f9a3a7f2bd
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-30 04:50:37 +0200

    Add task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,18 +6,15 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + id + '/';
 
-request.get(url, (error, res, body) => {
-  if (error) {
-    console.error('error:', error);
-  }
-  const characters = JSON.parse(body).characters;
-  console.log(characters);
-  characters.forEach((character) => {
-    request.get(character, (error, res, body) => {
-      if (error) {
-        console.error('error:', error);
-      }
-      console.log(JSON.parse(body).name);
-    });
-  });
-});
+request(url)
+  .than((error, res, body) => {
+    if (error) {
+      console.error('error:', error);
+    }
+    const characters = JSON.parse(body).characters;
+    console.log(characters);
+    return characters;
+  })
+  .then(characters => Promise.all(characters.map(char => request(char))))
+  .then(characters => characters.map(char => JSON.parse(char.body).name))
+  .then(names => names.forEach(name => console.log(name)));
